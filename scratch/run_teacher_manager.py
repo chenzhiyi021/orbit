fdd402a4647759f25f7cc371c5a16d730e11bfdd
chenzhiyi021 <chenzhiyi021@gmail.com>
2026-06-22 20:54:13 +0800

    feat: update run_teacher_manager

--- scratch/run_teacher_manager.py
+++ scratch/run_teacher_manager.py
@@ -1,6 +1,9 @@
+import sys
+
 import ray
 from ray.util.placement_group import placement_group
-from argparse import Namespace
+
+from orbit.utils.arguments import parse_args
 
 ray.init(num_gpus=1)
 
@@ -9,14 +12,15 @@ ray.get(pg.ready())
 
 print(f"Placement group created with bundles: {pg}")
 
-args = Namespace(
-    opd_teacher_model_path="/mnt/L202500431/model/qwen2.5-0.5b-instruct",
-    opd_teacher_tp_size=1,
-    hf_checkpoint="/mnt/L202500431/model/qwen2.5-0.5b-instruct",
-    num_gpus_per_node = 1,
-    sglang_dp_size = 1,
-    env_report = "none",
-)
+sys.argv = [
+    "",  
+    "--hf-checkpoint", "/mnt/L202500431/model/qwen2.5-0.5b-instruct",
+    "--rollout-batch-size", "1",
+]
+
+args = parse_args()
+args.opd_teacher_model_path = "/mnt/L202500431/model/qwen2.5-0.5b-instruct"
+args.opd_teacher_tp_size = 1
 
 from orbit.ray.teacher import TeacherManager
 manager = TeacherManager.remote(args, (pg, [0], [0]))
